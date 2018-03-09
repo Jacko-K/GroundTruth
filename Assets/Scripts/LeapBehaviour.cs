b43fbd7e107d889a30650c0cb6301eeb922f05ae
QUTAD\kimberlj <j.kimberley@qut.edu.au>
2018-03-09 16:50:12 +1000

    Audio conrols updated, with reverse sounds

--- Assets/Scripts/LeapBehaviour.cs
+++ Assets/Scripts/LeapBehaviour.cs
@@ -15,13 +15,10 @@ public class LeapBehaviour : MonoBehaviour
     public float yDeadzone = 0.01f;
     public AudioMixer a;
     public AudioMixerSnapshot normal;
-    public AudioMixerSnapshot fastTime;
-    public AudioMixerSnapshot slowTime;
-    public AudioMixerSnapshot falseColor;
-    public AudioMixerSnapshot trueColor;
     public AudioMixerSnapshot[] soundSnapshots;
     public float[] weights;
-    private Vector2 startPos = Vector2.zero;
+    public float mixerSpeed;
+    private Vector2 startPos = new Vector2(0, 0.3f);
     private int leftRight;
     private int upDown;
 
@@ -39,30 +36,39 @@ public class LeapBehaviour : MonoBehaviour
         if (frame.Hands.Count > 0)
         {
             ci.hasUser = true;
-            if (startPos != Vector2.zero)
-            {
+            //if (startPos != Vector2.zero)
+            //{
                 Vector2 diff = new Vector2(frame.Hands[0].PalmPosition.x - startPos.x, frame.Hands[0].PalmPosition.y - startPos.y);
 
                 if (Mathf.Abs(diff.x) > xDeadzone)
                 {
                     if (diff.x < 0 && frame.Hands[0].PalmPosition.x < xCutoff)
                     {
-                        Debug.Log("left: " + diff.x);
+                        //Debug.Log("left: " + diff.x);
                         ci.BlendX((frame.Hands[0].PalmPosition.x - xCutoff) * LeapMultiplier);
-                        //slowTime.TransitionTo(2f);
                         leftRight = 1;
+                        foreach(AudioSource a in GetComponents<AudioSource>())
+                        {
+                           a.pitch = Mathf.Lerp(a.pitch, -1f, 0.05f);
+                        }
                     }
                     else if (diff.x > 0 && frame.Hands[0].PalmPosition.x > xCutoff)
                     {
-                        Debug.Log("right: " + diff.x);
+                        //Debug.Log("right: " + diff.x);
                         ci.BlendX((frame.Hands[0].PalmPosition.x - xCutoff) * LeapMultiplier);
-                        //fastTime.TransitionTo(2f);
                         leftRight = 2;
-              
+                        foreach (AudioSource a in GetComponents<AudioSource>())
+                        {
+                           a.pitch = Mathf.Lerp(a.pitch, 1f, 0.05f);
+                        }
                     }
                     else
                     {
                         leftRight = 0;
+                        foreach (AudioSource a in GetComponents<AudioSource>())
+                        {
+                            a.pitch = Mathf.Lerp(a.pitch, 1f, 0.05f);
+                        }
                     }
                 }
 
@@ -70,16 +76,14 @@ public class LeapBehaviour : MonoBehaviour
                 {
                     if (diff.y > 0 && frame.Hands[0].PalmPosition.y > yCutoff)
                     {
-                        Debug.Log("up: " + diff.y);
+                        //Debug.Log("up: " + diff.y);
                         ci.BlendY((frame.Hands[0].PalmPosition.y - yCutoff) * LeapMultiplier);
-                        //falseColor.TransitionTo(2f);
                         upDown = 1;
                     }
                     else if (diff.y < 0 && frame.Hands[0].PalmPosition.y < yCutoff)
                     {
-                        Debug.Log("down: " + diff.y);
+                        //Debug.Log("down: " + diff.y);
                         ci.BlendY((frame.Hands[0].PalmPosition.y - yCutoff) * LeapMultiplier);
-                        //trueColor.TransitionTo(2f);
                         upDown = 2;
                     }
                     else
@@ -108,21 +112,25 @@ public class LeapBehaviour : MonoBehaviour
                 //    ci.BlendX((frame.Hands[0].PalmPosition.x - xCutoff) * LeapMultiplier);
                 //    slowTime.TransitionTo(2f);
                 //}
-            }
-            else
-            {
-                startPos = new Vector2(frame.Hands[0].PalmPosition.x, frame.Hands[0].PalmPosition.y);
-                Debug.Log("start: " + startPos);
-            }
+            //}
+            //else
+            //{
+            //    startPos = new Vector2(frame.Hands[0].PalmPosition.x, frame.Hands[0].PalmPosition.y);
+            //    Debug.Log("start: " + startPos);
+            //}
         }
-        else if (startPos != Vector2.zero)
+        else// if (startPos != Vector2.zero)
         {
-            Debug.Log("RESET: " + startPos);
-            startPos = Vector2.zero;
+            //Debug.Log("RESET: " + startPos);
+            //startPos = Vector2.zero;
             ci.hasUser = false;
             normal.TransitionTo(2f);
             leftRight = 0;
             upDown = 0;
+            foreach (AudioSource a in GetComponents<AudioSource>())
+            {
+                a.pitch = 1f;
+            }
         }
 
         BlendSound(leftRight, upDown);
@@ -137,7 +145,7 @@ public class LeapBehaviour : MonoBehaviour
             weights[1] = 0f;
             weights[2] = 0f;
             weights[3] = 0f;
-            a.TransitionToSnapshots(soundSnapshots, weights, 2f);
+            a.TransitionToSnapshots(soundSnapshots, weights, mixerSpeed);
         }
         else if(blendX == 0 && blendY == 2)
         {
@@ -145,7 +153,7 @@ public class LeapBehaviour : MonoBehaviour
             weights[1] = 1f;
             weights[2] = 0f;
             weights[3] = 0f;
-            a.TransitionToSnapshots(soundSnapshots, weights, 2f);
+            a.TransitionToSnapshots(soundSnapshots, weights, mixerSpeed);
         }
         else if(blendX == 1 && blendY == 0)
         {
@@ -153,7 +161,7 @@ public class LeapBehaviour : MonoBehaviour
             weights[1] = 0f;
             weights[2] = 1f;
             weights[3] = 0f;
-            a.TransitionToSnapshots(soundSnapshots, weights, 2f);
+            a.TransitionToSnapshots(soundSnapshots, weights, mixerSpeed);
         }
         else if(blendX == 2 && blendY == 0)
         {
@@ -161,7 +169,7 @@ public class LeapBehaviour : MonoBehaviour
             weights[1] = 0f;
             weights[2] = 0f;
             weights[3] = 1f;
-            a.TransitionToSnapshots(soundSnapshots, weights, 2f);
+            a.TransitionToSnapshots(soundSnapshots, weights, mixerSpeed);
         }
         else if(blendX == 1 && blendY == 1)
         {
@@ -169,7 +177,7 @@ public class LeapBehaviour : MonoBehaviour
             weights[1] = 0f;
             weights[2] = 0.5f;
             weights[3] = 0f;
-            a.TransitionToSnapshots(soundSnapshots, weights, 2f);
+            a.TransitionToSnapshots(soundSnapshots, weights, mixerSpeed);
         }
         else if(blendX == 2 && blendY == 1)
         {
@@ -177,7 +185,7 @@ public class LeapBehaviour : MonoBehaviour
             weights[1] = 0f;
             weights[2] = 0f;
             weights[3] = 0.5f;
-            a.TransitionToSnapshots(soundSnapshots, weights, 2f);
+            a.TransitionToSnapshots(soundSnapshots, weights, mixerSpeed);
         }
         else if(blendX == 1 && blendY == 2)
         {
@@ -185,7 +193,7 @@ public class LeapBehaviour : MonoBehaviour
             weights[1] = 0.5f;
             weights[2] = 0.5f;
             weights[3] = 0f;
-            a.TransitionToSnapshots(soundSnapshots, weights, 2f);
+            a.TransitionToSnapshots(soundSnapshots, weights, mixerSpeed);
         }
         else if(blendX == 2 && blendY == 2)
         {
@@ -193,7 +201,7 @@ public class LeapBehaviour : MonoBehaviour
             weights[1] = 0.5f;
             weights[2] = 0f;
             weights[3] = 0.5f;
-            a.TransitionToSnapshots(soundSnapshots, weights, 2f);
+            a.TransitionToSnapshots(soundSnapshots, weights, mixerSpeed);
         }
     }
 }
